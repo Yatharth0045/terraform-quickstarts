a6e5258d08fd1c322d581614f6d90a3665b1a00c
Yatharth Sharma <yatharth0045@gmail.com>
2024-09-06 06:37:04 +0530

    added provider source

--- dependency-graph/provider.tf
+++ dependency-graph/provider.tf
@@ -2,9 +2,11 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     local = {
+      source  = "hashicorp/local"
       version = "2.5.1"
     }
     random = {
+      source  = "hashicorp/random"
       version = "3.6.2"
     }
   }
--- variable-precedence/provider.tf
+++ variable-precedence/provider.tf
@@ -2,18 +2,12 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     local = {
+      source  = "hashicorp/local"
       version = "2.5.1"
     }
     random = {
+      source  = "hashicorp/random"
       version = "3.6.2"
     }
   }
 }
-
-# provider "local" {
-
-# }
-
-# provider "random" {
-
-# }
